8a33586aecb67e132c6a38f939b2c2236576c5ba
guiferviz <guiferviz@gmail.com>
2016-08-17 00:46:35 +0200

    Main state machine. Adding basic background movement.

--- main.lua
+++ main.lua
@@ -9,9 +9,16 @@ W = 1280
 H = 720
 
 
+-- Game properties.
+state = "menu"
+
+
 function love.load()
     love.window.setMode(W, H, {resizable=true})
     Screen.set(W, H)
+
+    back = WorldObject("images/back.png")
+    back:setSize(W, H)
 end
 
 function love.resize(w, h)
@@ -19,22 +26,61 @@ function love.resize(w, h)
 end
 
 function love.update(dt)
-    
+    if state == "menu" then
+        updateMenu(dt)
+    elseif state == "game" then
+        updateGame(dt)
+    end
 end
 
 function love.draw()
     Screen.transform()
 
-    love.graphics.setColor(100, 144, 24)
-    love.graphics.rectangle("fill", 0, 0, W, H)
-    love.graphics.setColor(140, 44, 24)
-    love.graphics.rectangle("line", 0, 0, W/2, H/2)
+    if state == "menu" then
+        drawMenu()
+    elseif state == "game" then
+        drawGame()
+    end
 
     Screen.drawBorders()
+end
+
+function love.mousereleased(x, y, button, istouch)
+    if state == "menu" then
+        state = "game"
+    elseif state == "game" then
+        state = "menu"
+    end
 end
 
 function love.keyreleased(key)
     if key == "escape" then
         love.event.quit()
+    end
+end
+
+
+function updateMenu(dt)
+    moveBackground(50 * dt)
+end
+
+function updateGame(dt)
+    moveBackground(150 * dt)
+end
+
+function drawMenu()
+    drawGame()
+end
+
+function drawGame()
+    back:draw()
+end
+
+
+function moveBackground(stepSize)
+    back.x = back.x - stepSize
+
+    if back.x < -W then
+        back.x = 0
     end
 end
